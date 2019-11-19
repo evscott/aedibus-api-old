d2db7e5379e084c51b6f4e96216847a565663532
Eliot Scott <eliotvscott@gmail.com>
2019-11-18 23:25:10 -0400

    Cleanup

--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,6 @@
 package Routes
 
 import (
-	"github.com/bndr/gojenkins"
 	"github.com/evscott/z3-e2c-api/routes/handlers"
 	"github.com/google/go-github/github"
 	"github.com/gorilla/mux"
@@ -12,7 +11,7 @@ type Config struct {
 	GithubRoutes *handlers.Config
 }
 
-func GetRoutes(router *mux.Router, jenkins *gojenkins.Jenkins, github *github.Client) *Config {
+func GetRoutes(router *mux.Router, github *github.Client) *Config {
 	c := &Config{
 		Router:       router,
 		GithubRoutes: &handlers.Config{GAL: github},
@@ -24,10 +23,24 @@ func GetRoutes(router *mux.Router, jenkins *gojenkins.Jenkins, github *github.Cl
 }
 
 func (c *Config) handleGithubRoutes() {
-	c.Router.HandleFunc(Path(Github, PullRequest, Comment), c.GithubRoutes.CreateComment).Methods(POST)
-	c.Router.HandleFunc(Path(Github, Repository), c.GithubRoutes.CreateRepository).Methods(POST)
-	c.Router.HandleFunc(Path(Github, Branch), c.GithubRoutes.CreateBranch).Methods(POST)
+	c.handleGeneralRoutes()
+	c.handleInstructorRoutes()
+}
+
+func (c *Config) handleGeneralRoutes() {
+	// Upload File
 	c.Router.HandleFunc(Path(Github, File), c.GithubRoutes.UploadFile).Methods(POST)
+	// Update File
 	c.Router.HandleFunc(Path(Github, File), c.GithubRoutes.UpdateFile).Methods(PUT)
+	// Create Pull Request
 	c.Router.HandleFunc(Path(Github, PullRequest), c.GithubRoutes.CreatePullRequest).Methods(POST)
+}
+
+func (c *Config) handleInstructorRoutes() {
+	// Create Repository
+	c.Router.HandleFunc(Path(Github, Repository), c.GithubRoutes.CreateRepository).Methods(POST)
+	// Create Branch
+	c.Router.HandleFunc(Path(Github, Branch), c.GithubRoutes.CreateBranch).Methods(POST)
+	// Create Comment on Pull Request
+	c.Router.HandleFunc(Path(Github, PullRequest, Comment), c.GithubRoutes.CreateComment).Methods(POST)
 }
