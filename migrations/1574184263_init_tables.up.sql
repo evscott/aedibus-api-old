5b307efb788e788361ba7153d7200e68567b3701
Eliot Scott <eliotvscott@gmail.com>
2019-11-20 13:17:08 -0400

    Add initial assignment/submission schemas.

--- migrations/1574184263_init_tables.up.sql
+++ migrations/1574184263_init_tables.up.sql
@@ -1,1 +1,14 @@
-CREATE TABLE IF NOT EXISTS test_table ();
+CREATE TABLE IF NOT EXISTS Assignments (
+    RepoName varchar (255) PRIMARY KEY,
+    MasterBranch varchar (255) NOT NULL,
+    LatestCommit varchar (40) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Submissions (
+    RepoName varchar (255) PRIMARY KEY REFERENCES Assignments(RepoName),
+    MasterBranch varchar (255) NOT NULL,
+    WorkingBranch varchar (255) NOT NULL,
+    LatestCommit varchar (40) NOT NULL,
+    Submitted bool DEFAULT FALSE,
+    Reviewed bool DEFAULT FALSE
+);
