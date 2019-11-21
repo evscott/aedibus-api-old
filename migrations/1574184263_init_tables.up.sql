77e0669ed16c0e21e56196f808d9c66d79f2f243
Eliot Scott <eliotvscott@gmail.com>
2019-11-21 13:18:29 -0400

    Add bare bones File migration

--- migrations/1574184263_init_tables.up.sql
+++ migrations/1574184263_init_tables.up.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS Assignments (
     name varchar (255) PRIMARY KEY,
     branch varchar (255) NOT NULL,
@@ -5,8 +7,16 @@ CREATE TABLE IF NOT EXISTS Assignments (
 );
 
 CREATE TABLE IF NOT EXISTS Submissions (
-    assignment varchar (255) PRIMARY KEY REFERENCES Assignments(name),
+    id uuid UNIQUE DEFAULT uuid_generate_v1(),
+    assignment varchar (255) REFERENCES Assignments(name),
     branch varchar (255) NOT NULL,
     submitted bool DEFAULT FALSE,
-    grade bool DEFAULT FALSE
+    grade bool DEFAULT FALSE,
+    PRIMARY KEY (id, assignment)
+);
+
+CREATE TABLE IF NOT EXISTS File (
+    submissions_id uuid REFERENCES Submissions(id),
+    file_name varchar(255) NOT NULL,
+    PRIMARY KEY (submissions_id, file_name)
 );
